2682e72987b0e1f5c9fe0ff42b866647660b8797
Matteo0808 <robloxscreemer@gmail.com>
2026-08-29 20:32:53 +0300

    DONE

--- docs/html/search/defines_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['red_5ftext_86',['RED_TEXT',['../_config_8h.html#ac6b49286f191080eef7f1891a32b6596',1,'Config.h']]]
-];
--- docs/html/search/variables_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['c_71',['c',['../struct_coefficients.html#a3a8a6e5cc52fbaa075ef61983d441eed',1,'Coefficients']]],
-  ['case_5fsolution_72',['case_solution',['../struct_roots.html#ad43c6e099a5cbf6ad224e3896cf33645',1,'Roots']]],
-  ['coeffs_73',['coeffs',['../struct_equation.html#a6186c1ee9a7a216f8f34cf4c637e7be4',1,'Equation']]]
-];
